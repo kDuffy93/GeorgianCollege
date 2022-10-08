eb0731e0b77d88e81385554cedb7204e07a833b6
Kyle Duffy <kyleduffy83@gmail.com>
2022-10-08 15:11:14 -0400

    color scheme setup
    color scheme set

--- F22/Advanced-Interface-using-CSS-COMP2081/Assignment1/scripts/scripts.js
+++ F22/Advanced-Interface-using-CSS-COMP2081/Assignment1/scripts/scripts.js
@@ -1,27 +1,38 @@
 let footerTarget = document.getElementById("mainContent2");
 let footer = document.getElementById("footer");
-let headerTarget = document.getElementById("secondaryContent1");
+let headerTarget = document.getElementById("mainBanner");
 let header = document.getElementById("header");
+let nav = document.getElementById("nav");
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        const intersecting = entry.isIntersecting;
-        intersecting
-            ?
-            footer.classList.add("atBottom") :
-            footer.classList.remove("atBottom");
-    });
-});
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            const intersecting = entry.isIntersecting;
+            intersecting
+                ?
+                footer.classList.add("atBottom") :
+                footer.classList.remove("atBottom");
+        });
+    }, {
+        threshold: [.4],
+    }
+);
 observer.observe(footerTarget);
 
 
-const headerObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        const intersecting = entry.isIntersecting;
-        !intersecting
-            ?
-            header.classList.add("scrolling") :
-            header.classList.remove("scrolling");
-    });
-});
+const headerObserver = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            const intersecting = entry.isIntersecting;
+            !intersecting
+                ?
+                header.classList.add("scrolling") & nav.classList.add("navScrolling") :
+                header.classList.remove("scrolling") &
+                nav.classList.remove("navScrolling");
+        });
+    }, {
+        threshold: [1],
+    }
+);
 headerObserver.observe(headerTarget);
