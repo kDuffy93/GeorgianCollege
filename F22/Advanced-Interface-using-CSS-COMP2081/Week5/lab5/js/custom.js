6928dc9e81fa3bbea9e47871c7dc986a9a5fb9fe
Kyle Duffy <kyleduffy83@gmail.com>
2022-10-15 00:02:57 -0400

    lab 5 complete
    followed step 1-5 to comeplete lab using in class files.

--- F22/Advanced-Interface-using-CSS-COMP2081/Week5/lab5/js/custom.js
+++ F22/Advanced-Interface-using-CSS-COMP2081/Week5/lab5/js/custom.js
@@ -4,13 +4,21 @@ const carouselCars = document.getElementById("carousel-cars");
 const carouselCaption = document.querySelectorAll("figcaption");
 
 carouselCars.addEventListener('slide.bs.carousel', event => {
-  // do something...
+    // do something...
     carouselCars.classList.add("zoomit");
 });
 carouselCars.addEventListener('slid.bs.carousel', event => {
     // do something...
-      carouselCars.classList.remove("zoomit");
+    carouselCars.classList.remove("zoomit");
 });
-  
+
+//LAB STEP 5
+var carousel = new bootstrap.Carousel(carouselCars, {
+    keyboard: false,
+});
+
+
+
+
 // STEP 16: Change the interval to 5 seconds (set in milliseconds)
 // STEP 17: Try an event handler to zoom up the .carousel-caption after the slide completes sliding - by adding a class when the slide has finished animating, and removing it when it begins
