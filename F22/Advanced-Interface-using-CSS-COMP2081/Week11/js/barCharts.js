3473f5d7e64de8cab347c78a3da26000c86974c8
Kyle Duffy <kyleduffy83@gmail.com>
2022-11-25 22:43:28 -0500

    code Generator portion
    added a code generator that prints out the svg code and allows for simple copying to clipboard

--- F22/Advanced-Interface-using-CSS-COMP2081/Week11/js/barCharts.js
+++ F22/Advanced-Interface-using-CSS-COMP2081/Week11/js/barCharts.js
@@ -4,6 +4,9 @@ const input = document.querySelector("input");
 const svgns = "http://www.w3.org/2000/svg";
 let inputs = document.getElementById("barValues");
 const group = document.querySelector("g");
+const svgCodeOutput = document.getElementById("svgCodeOutput");
+const sampleOutput = document.getElementById("sampleOutput");
+
 
 let buildBar = (numberOfBars, i) => {
     let tempRect = document.createElementNS(svgns, "rect");
@@ -14,33 +17,56 @@ let buildBar = (numberOfBars, i) => {
     tempRect.setAttribute("y", `90%`); //horizontal offset
     tempRect.setAttribute("height", `10%`); //horizontal offset
     tempRect.style = `--barWidth: ${x * 0.75}%;`;
-
     tempRect.setAttribute("title", `Title ${i + 1} - 10%`); //horizontal offset
     tempRect.setAttribute("fill", "#5cceee");
+    //    console.log(`tempRect: ${tempRect}`);
 
-    console.log(`tempRect: ${tempRect}`);
+    let tempBarText = document.createElementNS(svgns, "text");
+    tempBarText.setAttribute("x", `${x * i + x * 0.3}%`); //horizontal offset
+    tempBarText.setAttribute("textLength", `${x * 0.6}%`); //width
+    tempBarText.setAttribute("y", `95%`); //horizontal offset
+    tempBarText.textContent = `10%`;
+    tempBarText.setAttribute("fill", "#000");
+    tempBarText.textAlign = "center";
+    tempBarText.style.zIndex = "10";
 
     let tempText = document.createElementNS(svgns, "text");
     tempText.setAttribute("x", `${x * i + x * 0.225}%`); //horizontal offset
     tempText.setAttribute("textLength", `${x * 0.6}%`); //width
     tempText.setAttribute("y", `110%`); //horizontal offset
     tempText.textContent = `Title ${i + 1}`;
+    tempText.setAttribute("fill", "#000");
+    tempText.textAlign = "center";
+
+
+
     if (numberOfBars > 10) {
         tempText.setAttribute("x", `${x * i + x * 0.5}%`);
         tempText.setAttribute("textLength", `200`); //horizontal offset
         tempText.style.writingMode = "vertical-lr";
         tempText.style.textOrientation = "upright";
+
+        //tempBarText;
+        tempBarText.setAttribute("x", `${x * i + x * 0.5}%`);
+        tempBarText.style.writingMode = "vertical-lr";
+        tempBarText.setAttribute("textLength", `50`); //horizontal offset
+        tempBarText.style.textOrientation = "upright";
+        tempBarText.style.fontSize = "smaller";
+        tempBarText.setAttribute("y", `90%`);
     }
-    tempText.setAttribute("fill", "#000");
-    tempText.textAlign = "center";
+
+
+
+
 
     group.appendChild(tempText);
     group.appendChild(tempRect);
+    group.appendChild(tempBarText);
 
-    return tempRect;
-};;;
+    return { bar: tempRect, barText: tempBarText, barNumber: numberOfBars };
+};
 
-let buildInput = (bar, i) => {
+let buildInput = ({ bar, barText, barNumber }, i) => {
     console.log(`bar: ${bar}`);
     console.log();
     let tempContainer = document.createElement("div");
@@ -57,7 +83,15 @@ let buildInput = (bar, i) => {
         let titleWords = bar.getAttribute("title").split(" - ");
         titleWords[0] = `${String(e.target.value)}`;
         bar.setAttribute("title", `${titleWords[0]} - ${titleWords[1]}`); //horizontal offset
-    };;
+
+        let svgCode = document.getElementById("svgDiv").innerHTML;
+        svgCode.replaceAll('</text>', '<text> /n');
+        svgCode.replaceAll("</rect>", "<rect> /n");
+        svgCodeOutput.textContent = "";
+        sampleOutput.innerHTML = "";
+
+
+    };
 
 
     let tempNameLabel = document.createElement("label");
@@ -88,14 +122,26 @@ let buildInput = (bar, i) => {
             bar.setAttribute("height", `${negatedNalue}%`); //horizontal offset
             let titleWords = bar.getAttribute("title").split(" - ");
             titleWords[1] = `${String(e.target.value)}%`;
-            bar.setAttribute("title", `${titleWords[0]} - ${titleWords[1]}`); //horizontal offset
+            bar.setAttribute("title", `${titleWords[0]} - ${titleWords[1]}`);
+            barText.textContent = `${e.target.value}%`;
+            barText.setAttribute("y", `${100 - e.target.value / 2}%`);
+            svgCodeOutput.textContent = "";
+            sampleOutput.innerHTML = "";
+
         } else {
             bar.setAttribute("y", `${100 - e.target.value}%`); //horizontal offset
             bar.setAttribute("height", `${e.target.value}%`); //horizontal offset
-
             let titleWords = bar.getAttribute("title").split(" - ");
-            titleWords[1] = `${String(e.target.value)}%`
+            titleWords[1] = `${String(e.target.value)}%`;
             bar.setAttribute("title", `${titleWords[0]} - ${titleWords[1]}`); //horizontal offset
+            barText.setAttribute("y", `${100 - (e.target.value/2)}%`); //horizontal offset;
+            if (barNumber > 10) {
+                barText.setAttribute("y", `${(100 - ((e.target.value < 100 ? e.target.value : 100 - (e.target.value/4))) )}%`); //horizontal offset;
+            }
+
+            barText.textContent = `${e.target.value}%`;
+            svgCodeOutput.innerHTML = "";
+            sampleOutput.innerHTML = "";
         }
 
     };
@@ -120,13 +166,58 @@ let reloadSVG = () => {
         for (let i = 0; i < numberOfBars; i++) {
 
             buildInput(buildBar(numberOfBars, i), i);
+
         }
     }
+
+
+
 
 }
 
 reloadSVG();
 
+
 input.onchange = () => {
     reloadSVG();
 };
+
+let generateCodeButton = document.querySelector("body>aside>button:first-of-type");
+generateCodeButton.addEventListener("click", (e) => {
+    let svgCode = document.getElementById("svgDiv").innerHTML;
+    let svgOutput = document.getElementById("sampleOutput");
+
+
+    let cleanSvgCode = svgCode.replaceAll('</text>', `</text>
+                    `).replaceAll('</rect>', `</rect>
+                    `).slice().replace(`           <script src="js/barCharts.js"></script>
+        `, `       `).replace('<g>', `<g>
+                    `).replace("    </g>", `</g>`);
+    svgOutput.innerHTML = cleanSvgCode;
+    svgCodeOutput.textContent = cleanSvgCode;
+})
+
+let copyCodeButton = document.querySelector("body>aside>button:last-of-type");
+copyCodeButton.addEventListener("click", (e) => {
+    let svgCode = document.getElementById("svgDiv").innerHTML;
+    let cleanSvgCode = svgCode.replaceAll('</text>', `</text>
+                    `).replaceAll('</rect>', `</rect>
+                    `).slice().replace(`           <script src="js/barCharts.js"></script>
+        `, `       `).replace('<g>', `<g>
+                    `).replace("    </g>", `</g>`);
+    navigator.clipboard
+        .writeText(cleanSvgCode)
+
+    .then(
+        function() {
+            console.log("Async: Copying to clipboard was successful!");
+        },
+        function(err) {
+            console.error("Async: Could not copy text: ", err);
+        }
+    );
+    copyCodeButton.textContent = "Copied";
+    setTimeout(() => {
+        copyCodeButton.innerHTML = `<i class="fa-regular fa-copy"></i> Copy To Clipboard`;
+    }, 1000);
+});
